3118e81de0efc8a6dea75b5aee0895faa77df08f
Theta Nil, LLC <thetanil@thetanil.com>
2021-12-13 14:33:37 +0100

    fail test

--- tests/test_evergreen.py
+++ tests/test_evergreen.py
@@ -1,9 +1,19 @@
 import pytest
 
 
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+
+    # def test_two(self):
+    #     x = {"check": True}
+    #     assert hasattr(x, "check")
+
+
 def test_answer():
     assert 1 == 1
 
 
-def always_fail():
+def test_fail():
     assert 1 == 2
