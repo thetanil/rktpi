64596f2072d42cc173e34f80fe3e0fd19421e6c0
Theta Nil, LLC <thetanil@thetanil.com>
2021-12-14 22:52:32 +0100

    config

--- tasks.py
+++ tasks.py
@@ -3,6 +3,15 @@ from celery import Celery
 # app = Celery("tasks", broker="pyamqp://guest@localhost//")
 app = Celery("tasks", backend="rpc://", broker="pyamqp://")
 
+# https://docs.celeryproject.org/en/stable/getting-started/first-steps-with-celery.html
+app.conf.update(
+    task_serializer="json",
+    accept_content=["json"],  # Ignore other content
+    result_serializer="json",
+    timezone="Europe/Berlin",
+    enable_utc=True,
+)
+
 
 @app.task
 def add(x, y):
